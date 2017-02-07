2f06ee7f35f717728fc570a4c879cceb558b7781
Paul Osborne <posborne@ast.cam.ac.uk>
2017-02-07 21:54:52 +0000

    Completed best.R.

---
+++ best.R
@@ -0,0 +1,33 @@
+best <- function(state, outcome) {
+
+        data <- read.csv("./data/outcome-of-care-measures.csv", colClasses = "character")
+        cols <- list(
+                "heart attack" = 11L,
+                "heart failure" = 17L,
+                "pneumonia" = 23L
+        )
+        allowedStates <- sort(unique(data$State))
+        allowedOutcomes <- names(cols)
+        
+        if (!is.element(state, allowedStates)) stop("invalid state")       
+        if (!is.element(outcome, allowedOutcomes)) stop("invalid outcome")
+
+        subsetForOutcome <- function() {
+                d <- data.frame(
+                        data[,2],
+                        data[,7],
+                        suppressWarnings(
+                                as.numeric(
+                                        data[,cols[[outcome]]]
+                                )
+                        )
+                )
+                names(d) <- c("name","state", "score")                
+                d[!is.na(d$score) & d$state==state, ]
+        }
+        
+        s <- subsetForOutcome()
+        ranked <- s[with(s, order(score, name)),]
+        as.character(ranked$name[1])
+        
+}
